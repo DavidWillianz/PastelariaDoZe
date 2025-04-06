4c5331d247724a59dfce5ce01cf3b39c73183014
David <davidvieir22017@gmail.com>
2025-04-06 19:38:27 -0300

    wip

--- scr/main.py
+++ scr/main.py
@@ -40,22 +40,19 @@ app.include_router(ClienteDAO.router)
 app.include_router(ProdutoDAO.router)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host=HOST, port=int(PORT), reload=RELOAD)
+    import ssl
+    import hypercorn.asyncio
+    from hypercorn.config import Config
+    import asyncio
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile="PastelariaDoZe\cert\cert.pem", keyfile="PastelariaDoZe\cert\ecc-key.pem")
+    config = Config()
+    config.bind = ["0.0.0.0:4443"]
+    config.quic_bind = ["0.0.0.0:4443"]
 
+    #config.insecure_bind = ["0.0.0.0:8000"]
+    config.certfile = "PastelariaDoZe\cert\cert.pem"
+    config.keyfile = "PastelariaDoZe\cert\ecc-key.pem"
+    config.alpn_protocols = ["h2","h3"]
 
-# if __name__ == "__main__":
-#     import ssl
-#     import hypercorn.asyncio
-#     from hypercorn.config import Config
-#     import asyncio
-#     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-#     ssl_context.load_cert_chain(certfile="PastelariaDoZe\cert\cert.pem", keyfile="PastelariaDoZe\cert\ecc-key.pem")
-#     config = Config()
-#     config.bind = ["0.0.0.0:4443"]
-#     config.quic_bind = ["0.0.0.0:4443"]
-
-#     #config.insecure_bind = ["0.0.0.0:8000"]
-#     config.certfile = "cert\cert.pem"
-#     config.keyfile = "cert\ecc-key.pem"
-#     config.alpn_protocols = ["h2","h3"]
-#     asyncio.run(hypercorn.asyncio.serve(app, config))
+    asyncio.run(hypercorn.asyncio.serve(app, config))
